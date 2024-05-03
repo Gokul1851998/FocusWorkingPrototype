8e132fa22f09446fa0ba93a9a01b31a70c3f82a8
gokul <gokulmjp@gmail.com>
2024-05-03 10:43:08 +0530

    changes on 03/05/2024 10:43

---
+++ src/containers/Home/Master/Account/AccountMaster/AccountDetails.jsx
@@ -0,0 +1,127 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import { primaryButtonColor, thirdColor } from "../../../../../config";
+
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&::before": {
+    display: "none",
+  },
+}));
+
+const AccordionSummary = styled((props) => (
+    <MuiAccordionSummary
+    sx={{height:"10px"}}
+      expandIcon={
+        props.expanded ? (
+          <RemoveCircleOutlineIcon
+            sx={{ fontSize: "1.5rem", color: primaryButtonColor }}
+          />
+        ) : (
+          <AddCircleOutlineIcon
+            sx={{ fontSize: "1.5rem", color: primaryButtonColor }}
+          />
+        )
+      }
+      {...props}
+    />
+  ))(({ theme }) => ({
+    color: primaryButtonColor,
+    backgroundColor: thirdColor,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    "& .MuiAccordionSummary-content": {
+      flexGrow: 1,
+    },
+  }));
+  
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
+export default function AccountDetails() {
+  const [expanded, setExpanded] = React.useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      <Accordion
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+       <AccordionSummary
+  aria-controls="panel1d-content"
+  id="panel1d-header"
+  className
+  expanded={expanded === "panel1"}
+>
+          <Typography>General</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+      >
+        <AccordionSummary
+          aria-controls="panel2d-content"
+          id="panel2d-header"
+          expanded={expanded === "panel2"}
+        >
+          <Typography>Settings</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary
+          aria-controls="panel3d-content"
+          id="panel3d-header"
+          expanded={expanded === "panel3"}
+        >
+          <Typography>Details</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
