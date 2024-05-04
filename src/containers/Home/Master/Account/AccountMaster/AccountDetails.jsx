6ca9acb7d0f2582588900bfd59a9e3600e7d428f
gokul <gokulmjp@gmail.com>
2024-05-04 09:36:25 +0530

    changes on 04/05/2024 09:36

--- src/containers/Home/Master/Account/AccountMaster/AccountDetails.jsx
+++ src/containers/Home/Master/Account/AccountMaster/AccountDetails.jsx
@@ -7,7 +7,15 @@ import MuiAccordionSummary from "@mui/material/AccordionSummary";
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import { primaryButtonColor, thirdColor } from "../../../../../config";
-import { TextField } from "@mui/material";
+import { Checkbox, FormControlLabel, TextField } from "@mui/material";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBInput,
+  MDBRow,
+} from "mdb-react-ui-kit";
+import AccountInput from "../../../../../components/Inputs/AccountInput";
 
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -43,7 +51,7 @@ const AccordionSummary = styled((props) => (
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
+  paddingLeft: theme.spacing(3),
   borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
@@ -69,68 +77,57 @@ export default function AccountDetails() {
           <Typography style={{ fontSize: "14px" }}>General</Typography>
         </AccordionSummary>
         <AccordionDetails>
-        <TextField
-          margin="normal"
-          size="small"
-          id="search"
-          label="Search"
-          autoComplete="off"
-          autoFocus
-         
-          sx={{
-            width: 200, // Adjust the width as needed
-            "& .MuiInputBase-root": {
-              height: 30, // Adjust the height of the input area
-            },
-            "& .MuiInputLabel-root": {
-              transform: "translate(10px, 5px) scale(0.9)", // Adjust label position when not focused
-            },
-            "& .MuiInputLabel-shrink": {
-              transform: "translate(14px, -9px) scale(0.75)", // Adjust label position when focused
-            },
-            "& .MuiInputBase-input": {
-              fontSize: '0.75rem', // Adjust the font size of the input text
-            },
-            "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-              borderColor: "currentColor", // Keeps the current border color
-            },
-            "&:hover .MuiOutlinedInput-notchedOutline": {
-              borderColor: "currentColor", // Optional: Keeps the border color on hover
-            },
-            
-          }}
-        />
-           <TextField
-          margin="normal"
-          size="small"
-          id="search"
-          label="Search"
-          autoComplete="off"
-          autoFocus
-         
-          sx={{
-            width: 200, // Adjust the width as needed
-            "& .MuiInputBase-root": {
-              height: 30, // Adjust the height of the input area
-            },
-            "& .MuiInputLabel-root": {
-              transform: "translate(10px, 5px) scale(0.9)", // Adjust label position when not focused
-            },
-            "& .MuiInputLabel-shrink": {
-              transform: "translate(14px, -9px) scale(0.75)", // Adjust label position when focused
-            },
-            "& .MuiInputBase-input": {
-              fontSize: '0.75rem', // Adjust the font size of the input text
-            },
-            "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-              borderColor: "currentColor", // Keeps the current border color
-            },
-            "&:hover .MuiOutlinedInput-notchedOutline": {
-              borderColor: "currentColor", // Optional: Keeps the border color on hover
-            },
-            
-          }}
-        />
+          <>
+            <div>
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Name" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Code" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Select Tree" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Credit Limit" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Account Type" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Credit Days" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Revision Date" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Cheque Discount Limit" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Rate of Interest" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="PDC Discount A/C" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Back A/C" />
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </div>
+          </>
         </AccordionDetails>
       </Accordion>
       <Accordion
@@ -145,12 +142,53 @@ export default function AccountDetails() {
           <Typography style={{ fontSize: "14px" }}>Settings</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <>
+            <div>
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Debit/Credit Proposal" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Debit/Credit Required" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Exchange Adjustment Gain A/C" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Exchange Adjustment Loss A/C" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Primary Account" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Default Currency" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Consolidation Method" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Payment Terms" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Reminder Terms" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Finance Charge Terms" />
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </div>
+          </>
         </AccordionDetails>
       </Accordion>
       <Accordion
@@ -165,12 +203,93 @@ export default function AccountDetails() {
           <Typography style={{ fontSize: "14px" }}>Details</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <>
+            <div>
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    {" "}
+                    <AccountInput label="Address" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    {" "}
+                    <AccountInput label="Tel No" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    {" "}
+                    <AccountInput label="Fax No" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="City" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Pin" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    {" "}
+                    <AccountInput label="Delivery Address" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    {" "}
+                    <AccountInput label="City" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    {" "}
+                    <AccountInput label="Pin" />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          sx={{ transform: "scale(0.75)", paddingTop: 2 }}
+                        />
+                      } // Reduce the size of the checkbox
+                      label="Send Email to Customer"
+                      sx={{
+                        "& .MuiFormControlLabel-label": {
+                          fontSize: "0.8rem", // Adjust the label font size
+                          color: "gray", // Change the label color to gray
+                          paddingTop: 1,
+                        },
+                      }}
+                    />
+                  </MDBCol>
+
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          sx={{ transform: "scale(0.75)", paddingTop: 2 }}
+                        />
+                      } // Reduce the size of the checkbox
+                      label="Allow Customer/Vendor Portal"
+                      sx={{
+                        "& .MuiFormControlLabel-label": {
+                          fontSize: "0.8rem", // Adjust the label font size
+                          color: "gray", // Change the label color to gray
+                          paddingTop: 1,
+                        },
+                      }}
+                    />
+                  </MDBCol>
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Email" />
+                  </MDBCol>
+                  <MDBCol lg="2" md="4" sm="6" xs="12">
+                    <AccountInput label="Password" />
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </div>
+          </>
         </AccordionDetails>
       </Accordion>
 
