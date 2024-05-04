b09eae26327f5e89844994a1ece27daa084fb179
gokul <gokulmjp@gmail.com>
2024-05-04 14:53:36 +0530

    changes

---
+++ src/containers/Home/Master/Account/CustomerVendor/CustomerVendor.jsx
@@ -0,0 +1,464 @@
+import React, { useState } from "react";
+import { Box, CssBaseline, IconButton, TextField, styled } from "@mui/material";
+import { Collapse, Button, CardBody, Card, Alert } from "reactstrap";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import Tree1 from "../../../../../components/Tree/Tree1";
+import {
+  primaryButtonColor,
+  primaryColor,
+  secondryColor,
+} from "../../../../../config";
+import { accountTree } from "../../../../../config/masterConfig";
+import TableAccounts from "../../../../../components/Tables/TableAccounts";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import HomeIcon from "@mui/icons-material/Home";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import EditIcon from "@mui/icons-material/Edit";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Backdrop from "@mui/material/Backdrop";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import SaveIcon from "@mui/icons-material/Save";
+import PrintIcon from "@mui/icons-material/Print";
+import ShareIcon from "@mui/icons-material/Share";
+import GroupsIcon from "@mui/icons-material/Groups";
+import DeleteSweepIcon from "@mui/icons-material/DeleteSweep";
+import EventBusyIcon from "@mui/icons-material/EventBusy";
+import FolderOpenIcon from "@mui/icons-material/FolderOpen";
+import SystemUpdateAltIcon from "@mui/icons-material/SystemUpdateAlt";
+import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import TransferWithinAStationIcon from "@mui/icons-material/TransferWithinAStation";
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info("You clicked a breadcrumb.");
+}
+
+const actions = [
+  { icon: <GroupAddIcon />, name: "Group" },
+  { icon: <DeleteSweepIcon />, name: "Delete All" },
+  { icon: <EventBusyIcon />, name: "Close Account" },
+  { icon: <FolderOpenIcon />, name: "Open Close Account" },
+  { icon: <SystemUpdateAltIcon />, name: "Mass Update" },
+  { icon: <HomeRepairServiceIcon />, name: "Customize Master" },
+  { icon: <SettingsApplicationsIcon />, name: "Customize View" },
+  { icon: <AccountTreeIcon />, name: "Customize Tree" },
+  { icon: <ReorderIcon />, name: "Backtrack" },
+  { icon: <TransferWithinAStationIcon />, name: "Transfer" },
+];
+
+export default function CustomerVendor(args) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [hide, setHide] = useState(false);
+  const [isInfo, setIsInfo] = useState(false);
+  const [infoHide, setInfoHide] = useState(false);
+  const [detailPage, setDetailPage] = useState(false);
+  const [more, setMore] = React.useState(false);
+  const handleMoreOpen = () => setMore(true);
+  const handleMoreClose = () => setMore(false);
+
+  const toggleOpen = () => {
+    setIsOpen(true);
+    setHide(true);
+  };
+  const toggleClose = () => {
+    setIsOpen(false);
+    setTimeout(() => {
+      setHide(false);
+    }, 400);
+  };
+
+  const infoPanelOpen = () => {
+    setIsInfo(true);
+    setInfoHide(true);
+  };
+
+  const infoPanelClose = () => {
+    setIsInfo(false);
+    setTimeout(() => {
+      setInfoHide(false);
+    }, 400);
+  };
+
+  const handleDetailPageOpen = () => {
+    setDetailPage(true);
+  };
+
+  const handleDetailPageClose = () => {
+    setDetailPage(false);
+  };
+
+  const breadcrumbs = [
+    <Link
+      underline="hover"
+      sx={{ display: "flex", alignItems: "center", fontSize: "1rem" }} // Reduce font size
+      key="1"
+      color="white"
+      onClick={handleClick}
+    >
+      <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+      Home
+    </Link>,
+    <Link
+      underline="hover"
+      key="2"
+      color="white"
+      sx={{ fontSize: "1rem" }}
+      onClick={handleClick}
+    >
+      Master
+    </Link>,
+    <Link
+      underline="hover"
+      key="3"
+      color="white"
+      sx={{ fontSize: "1rem" }}
+      onClick={handleClick}
+    >
+      Account
+    </Link>,
+    <Typography key="4" color="white" sx={{ fontSize: "1rem" }}>
+      Customer/Vendor Master
+    </Typography>,
+  ];
+
+  return (
+    <>
+      <CssBaseline />
+
+      <React.StrictMode>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            paddingLeft: 1.5,
+            paddingRight: 1.5,
+            zIndex: 1,
+            backgroundColor: secondryColor,
+          }}
+        >
+          <Stack spacing={2} sx={{ flex: 1 }}>
+            <Breadcrumbs
+              separator={<NavigateNextIcon fontSize="small" />}
+              aria-label="breadcrumb"
+              style={{ color: primaryButtonColor }}
+            >
+              {breadcrumbs}
+            </Breadcrumbs>
+          </Stack>
+
+          {!detailPage ? (
+            <Stack
+              direction="row"
+              alignItems="center"
+              spacing={1}
+              sx={{ flex: "0 0 auto" }}
+            >
+              <IconButton onClick={handleDetailPageOpen}
+                aria-label="New"
+                sx={{ fontSize: "0.8rem", padding: "0.5rem" }}
+              >
+                <Stack direction="column" alignItems="center">
+                  <AddIcon style={{ color: "white" }} />
+                  <Typography
+                    variant="caption"
+                    align="center"
+                    style={{ color: "white", fontSize: "0.6rem" }}
+                  >
+                    New
+                  </Typography>
+                </Stack>
+              </IconButton>
+             
+              <IconButton
+                aria-label="Edit"
+                sx={{ fontSize: "0.8rem", padding: "0.5rem" }}
+              >
+                <Stack direction="column" alignItems="center">
+                  <EditIcon style={{ color: "white" }} />
+
+                  <Typography
+                    variant="caption"
+                    align="center"
+                    style={{ color: "white", fontSize: "0.6rem" }}
+                  >
+                    Edit
+                  </Typography>
+                </Stack>
+              </IconButton>
+              <IconButton
+                aria-label="Delete"
+                sx={{ fontSize: "0.8rem", padding: "0.5rem" }}
+              >
+                <Stack direction="column" alignItems="center">
+                  <DeleteIcon style={{ color: "white" }} />
+                  <Typography
+                    variant="caption"
+                    align="center"
+                    style={{ color: "white", fontSize: "0.6rem" }}
+                  >
+                    Delete
+                  </Typography>
+                </Stack>
+              </IconButton>
+              <IconButton
+                aria-label="Close"
+                sx={{ fontSize: "0.8rem", padding: "0.5rem" }}
+              >
+                <Stack direction="column" alignItems="center">
+                  <CloseIcon style={{ color: "white" }} />
+                  <Typography
+                    variant="caption"
+                    align="center"
+                    style={{ color: "white", fontSize: "0.6rem" }}
+                  >
+                    Close
+                  </Typography>
+                </Stack>
+              </IconButton>
+            </Stack>
+          ) : (
+            <Stack
+              direction="row"
+              alignItems="center"
+              spacing={1}
+              sx={{ flex: "0 0 auto" }}
+            >
+              <IconButton
+                aria-label="Save"
+                sx={{ fontSize: "0.8rem", padding: "0.5rem" }}
+              >
+                <Stack direction="column" alignItems="center">
+                  <SaveIcon style={{ color: "white" }} />
+                  <Typography
+                    variant="caption"
+                    align="center"
+                    style={{ color: "white", fontSize: "0.6rem" }}
+                  >
+                    Save
+                  </Typography>
+                </Stack>
+              </IconButton>
+
+              <IconButton
+                aria-label="Clone"
+                sx={{ fontSize: "0.8rem", padding: "0.5rem" }}
+              >
+                <Stack direction="column" alignItems="center">
+                  <FileCopyIcon style={{ color: "white" }} />
+                  <Typography
+                    variant="caption"
+                    align="center"
+                    style={{ color: "white", fontSize: "0.6rem" }}
+                  >
+                    Clone
+                  </Typography>
+                </Stack>
+              </IconButton>
+
+              <IconButton onClick={handleDetailPageClose}
+                aria-label="Close"
+                sx={{ fontSize: "0.8rem", padding: "0.5rem" }}
+              >
+                <Stack direction="column" alignItems="center">
+                  <CloseIcon style={{ color: "white" }} />
+                  <Typography
+                    variant="caption"
+                    align="center"
+                    style={{ color: "white", fontSize: "0.6rem" }}
+                  >
+                    Close
+                  </Typography>
+                </Stack>
+              </IconButton>
+            </Stack>
+          )}
+
+        </Box>
+  {detailPage? (
+   null
+  ): (
+
+ 
+        <>
+        <SpeedDial
+              ariaLabel="SpeedDial basic example"
+              sx={{ position: "absolute", bottom: 25, right: 16 }}
+              icon={<SpeedDialIcon />}
+              direction="left"
+            >
+              {actions.map((action) => (
+                <SpeedDialAction
+                  key={action.name}
+                  icon={action.icon}
+                  tooltipTitle={action.name}
+                />
+              ))}
+            </SpeedDial>
+        <div style={{ display: "flex" }}>
+          {!hide ? (
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                height: isOpen ? null : 590,
+              }}
+            >
+              <Button
+                color="primary"
+                onClick={toggleOpen}
+                style={{
+                  marginBottom: "1rem",
+                  padding: "0.3rem",
+                  fontSize: "0.6rem",
+                  height: "5rem",
+                  borderRadius: "0 0.5rem 0.5rem 0",
+                }}
+              >
+                <KeyboardDoubleArrowRightIcon style={{ fontSize: "1rem" }} />
+              </Button>
+            </div>
+          ) : null}
+
+          <Collapse horizontal isOpen={isOpen} {...args}>
+            <Alert
+              style={{
+                width: 350,
+                boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.3)",
+                backgroundColor: primaryButtonColor,
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Tree1 items={accountTree} />
+
+                <Button
+                  color="primary"
+                  onClick={toggleClose}
+                  style={{
+                    marginBottom: "1rem",
+                    padding: "0.3rem",
+                    fontSize: "0.6rem",
+                    height: "5rem",
+                    borderRadius: "0.5rem 0 0 0.5rem",
+                  }}
+                >
+                  <KeyboardDoubleArrowLeftIcon style={{ fontSize: "1rem" }} />
+                </Button>
+              </div>
+            </Alert>
+          </Collapse>
+       
+      
+      
+          <TableAccounts />
+        </div>
+        <div style={{ position: "fixed", bottom: 20, right: 20, zIndex: 100 }}>
+          <Collapse
+            style={{ marginBottom: "0.3rem" }}
+            isOpen={isInfo}
+            {...args}
+          >
+            <Alert
+              style={{
+                width: 350,
+                boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.3)",
+                backgroundColor: primaryButtonColor,
+                flexDirection: "column", // Arrange children vertically
+                alignItems: "center",
+                marginBottom: "0.3rem",
+                position: "relative", // Add position relative to the Alert
+              }}
+            >
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={infoPanelClose}
+                style={{
+                  borderRadius: "0 0 0.5rem 0.5rem",
+                  height: "1.2rem",
+                  width: "8rem",
+                  marginBottom: "0.3rem",
+                  marginLeft: "5rem",
+                  top: 0, // Align to the top
+                  right: 0, // Align to the end
+                  transform: "translateY(-100%)", // Move the button to the top of the container
+                  display: "flex",
+                  justifyContent: "center", // Center horizontally
+                  alignItems: "center", // Center vertically
+                }}
+              >
+                <KeyboardDoubleArrowDownIcon style={{ fontSize: "1rem" }} />
+              </Button>
+              <Box
+                sx={{
+                  overflowX: "hidden",
+                  height: 550,
+                  flexGrow: 1,
+                  minWidth: 300,
+                  scrollbarWidth: "thin",
+                  zIndex: 100,
+                }}
+              >
+                <Typography
+                  sx={{ flex: "1 1 100%" }}
+                  variant="h6"
+                  id="tableTitle"
+                  component="div"
+                >
+                  Info Panel
+                </Typography>
+              </Box>
+            </Alert>
+          </Collapse>
+
+          {!infoHide ? (
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={infoPanelOpen}
+              style={{
+                borderRadius: "0.5rem 0.5rem 0 0",
+                height: "1.2rem",
+                width: "8rem",
+                marginBottom: "0.3rem",
+                marginRight: "5rem",
+                display: "flex",
+                justifyContent: "center", // Center horizontally
+                alignItems: "center", // Center vertically
+              }}
+            >
+              <KeyboardDoubleArrowUpIcon style={{ fontSize: "1rem" }} />
+            </Button>
+          ) : null}
+        </div>
+        </>
+         )}
+      </React.StrictMode>
+    </>
+  );
+}
--- src/pages/MasterPage/MasterPage.jsx
+++ src/pages/MasterPage/MasterPage.jsx
@@ -4,6 +4,8 @@ import SideBar from "../../components/SideBar/SideBar";
 import { Box, styled } from "@mui/material";
 import AccountMaster from "../../containers/Home/Master/Account/AccountMaster/AccountMaster";
 import Footer from "../../components/Footer/Footer";
+import { useLocation } from "react-router-dom";
+import CustomerVendor from "../../containers/Home/Master/Account/CustomerVendor/CustomerVendor";
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -14,13 +16,20 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 }));
 
 export default function MasterPage() {
+   const location = useLocation()
+   const id = location.state.id
   return (
     <>
       <Box sx={{ display: "flex", }}>
         <SideBar />
         <Box component="main" sx={{ flexGrow: 1 }}>
           <DrawerHeader />
-          <AccountMaster />
+          {id === 19? (
+        <AccountMaster />
+          ): (
+           <CustomerVendor />
+          )}
+  
         </Box>
         <Footer />
       </Box>
