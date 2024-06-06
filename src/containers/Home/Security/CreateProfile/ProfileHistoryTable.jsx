765736e94d8b8f08f86f50653d19fd4ea7624c8e
Dipin-websoullabs <websoullabsdipin@gmail.com>
2024-06-06 16:05:32 +0530

    UserTheme_06_06_4

--- src/containers/Home/Security/CreateProfile/ProfileHistoryTable.jsx
+++ src/containers/Home/Security/CreateProfile/ProfileHistoryTable.jsx
@@ -2,41 +2,46 @@ import React from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import { historyProfile } from '../../../../config/securityConfig';
 import { thirdColor } from '../../../../config';
+import { useTheme } from '../../../../config/themeContext';
 
-const cellStyle = {
-  padding: "0px",
-                        paddingLeft: "4px",
-                        border: " 1px solid #ddd",
-                        fontWeight: "600",
-                        font: "14px",
-                        
-                        color: "white",
-                        paddingTop: "3px",
-                        paddingBottom: "3px",
-}
-const headerCellStyle = {
-  ...cellStyle,
-  backgroundColor: thirdColor,
-  color: "#fff",
-};
-const bodyCell={
-  padding: "0px",
-  paddingLeft: "4px",
-  border: " 1px solid #ddd",
-  minWidth: "100px",
-  whiteSpace: "nowrap",
-  overflow: "hidden",
-  textOverflow: "ellipsis",
-}
+
 const ProfileHistoryTable = ({ onRowClick }) => {
   // Sample data
+
+  const { currentTheme } = useTheme();
+
+  const cellStyle = {
+    padding: "0px",
+                          paddingLeft: "4px",
+                          border: " 1px solid #ddd",
+                          fontWeight: "600",
+                          font: "14px",
+                          
+                          color: currentTheme.tableHeaderColor,
+                          paddingTop: "3px",
+                          paddingBottom: "3px",
+  }
+  const headerCellStyle = {
+    ...cellStyle,
+    backgroundColor: currentTheme.thirdColor,
+    color: currentTheme.tableHeaderColor,
+  };
+  const bodyCell={
+    padding: "0px",
+    paddingLeft: "4px",
+    border: " 1px solid #ddd",
+    minWidth: "100px",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  }
   
 
   return (
     <TableContainer component={Paper} sx={{maxHeight:"40vh"}}>
       <Table>
         <TableHead>
-          <TableRow style={{ backgroundColor: thirdColor, color: "#fff" }}>
+          <TableRow >
             <TableCell sx={headerCellStyle }></TableCell>
             <TableCell sx={headerCellStyle }>Profile Name</TableCell>
             <TableCell sx={headerCellStyle }>Date</TableCell>
--- src/containers/Home/Security/CreateProfile/ProfileHistoryTable2.jsx
+++ src/containers/Home/Security/CreateProfile/ProfileHistoryTable2.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import { thirdColor } from '../../../../config';
+import { useTheme } from '../../../../config/themeContext';
 
-const cellStyle = {
-  padding: "0px",
-                        paddingLeft: "4px",
-                        border: " 1px solid #ddd",
-                        fontWeight: "600",
-                        font: "14px",
-                        
-                        color: "white",
-                        paddingTop: "3px",
-                        paddingBottom: "3px",
-}
-const headerCellStyle = {
-  ...cellStyle,
-  backgroundColor: thirdColor,
-  color: "#fff",
-};
-const bodyCell={
-  padding: "0px",
-  paddingLeft: "4px",
-  border: " 1px solid #ddd",
-  minWidth: "100px",
-  whiteSpace: "nowrap",
-  overflow: "hidden",
-  textOverflow: "ellipsis",
-}
+
 
 const ProfileHistoryTable2 = ({ selectedRow }) => {
+
+  const { currentTheme } = useTheme();
+
+  const cellStyle = {
+    padding: "0px",
+                          paddingLeft: "4px",
+                          border: " 1px solid #ddd",
+                          fontWeight: "600",
+                          font: "14px",
+                          
+                          color: currentTheme.tableHeaderColor,
+                          paddingTop: "3px",
+                          paddingBottom: "3px",
+  }
+  const headerCellStyle = {
+    ...cellStyle,
+    backgroundColor: currentTheme.thirdColor,
+    color: currentTheme.tableHeaderColor,
+  };
+  const bodyCell={
+    padding: "0px",
+    paddingLeft: "4px",
+    border: " 1px solid #ddd",
+    minWidth: "100px",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  }
  
   return (
     <TableContainer component={Paper} sx={{maxHeight:"40vh",scrollbarWidth:"thin"}}>
